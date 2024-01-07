f3c1ffa2f888277afc67658a2db1237bbc93a5c4
dgstephenson <dgstephenson.econ@gmail.com>
2024-01-07 00:18:53 -0500

    background

--- src/patterns/tile.ts
+++ src/patterns/tile.ts
@@ -2,17 +2,16 @@ const canvas: HTMLCanvasElement = document.createElement('canvas')
 const context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D
 
 canvas.style.imageRendering = 'pixelated'
-
 const size = 100 // this needs to be an even number
 canvas.width = size
 canvas.height = size
 context.fillStyle = '#000000'
-context.strokeStyle = '#161616'
+context.strokeStyle = '#99999910'
 context.fillRect(0, 0, size, size)
 context.beginPath()
 context.rect(0, 0, size, size)
 context.lineJoin = 'miter'
-context.lineWidth = 1
+context.lineWidth = 4
 context.stroke()
 
 export const tile: CanvasPattern = context.createPattern(canvas, 'repeat') as CanvasPattern
